97e63ab64a206025fc2df297d198054beb9063eb
Waleed Ehab Rizk Gomma <waleedehab228@gmail.com>
2026-03-08 22:04:12 +0200

    Third commit with new class

--- Banking System C# (Console App)/Program.cs
+++ Banking System C# (Console App)/Program.cs
@@ -1,10 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public Queue<int> CloneQueue(Queue<int> queue)
+    {
+        var clone = new Queue<int>();
+        Clone(queue, clone, queue.Count);
+        return clone;
+    }
+
+    private void Clone(Queue<int> original, Queue<int> clone, int n)
+    {
+        if (n == clone.Count)
+        {
+            return;
+        }
+
+        int x = original.Dequeue();
+
+        clone.Enqueue(x);
+        original.Enqueue(x);
+
+        Clone(original, clone, n);
+    }
+}
 
 public class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("This is a new feature here.");
+        var solution = new Solution();
+        var original = new Queue<int>();
+
+        original.Enqueue(1);
+        original.Enqueue(2);
+        original.Enqueue(3);
+        original.Enqueue(4);
+
+        var clone = solution.CloneQueue(original);
+
+        foreach (var item in clone)
+        {
+            Console.WriteLine(item);
+        }
+
+        foreach (var item in original)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.ReadKey();
     }
 }
 
